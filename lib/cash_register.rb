e698bacfd5f152b7ec96002bfd3d61fb2dcbd458
Chaim Bryski <luzybry94@gmail.com>
2020-08-19 22:12:30 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,7 @@ class CashRegister
   end
   
   def add_item(title, price, quantity = 1)
-    self.items.concat(title * 3)
+    self.items.concat(title *
     self.total += price * quantity
   end
   
